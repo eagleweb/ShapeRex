eccd347e4fc926085f0c33b490778013e7a9ea3e
eagleweb <orel.viacheslav@gmail.com>
2018-11-18 21:46:16 +0200

    Start commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component} from 'react'
-import { Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom'
 import createBrowserHistory from 'history/createBrowserHistory'
+import s from './App.module.css'
+import Header from './component/Header/Header'
 
 const history = createBrowserHistory();
-
-const Home = () => (
-    <div>
-        <h2>Home sder</h2>
-    </div>
-);
 
 export default class App extends Component {
     render(){
         return(
-            <Router history={history}>
-                <Route exact path="/" component={Home}/>
-            </Router>
+            <div className={s.wrapper}>
+                <Router history={history}>
+                    <Route exact path="/" component={Header}/>
+                </Router>
+            </div>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 const port = process.env.PORT || 3000;
 
@@ -14,14 +15,50 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(png|jpg|gif|svg|ico)$/,
+                loader: 'file-loader',
+                query:{
+                    outputPath: './img/',
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: 'file-loader',
+                query: {
+                    outputPath: './fonts/',
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
                 test: /\.(js)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
             },
             {
                 test: /\.css$/,
-                use: [ 'style-loader', 'css-loader']
-            }
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    browsers:['ie >= 11', 'last 4 version']
+                                })
+                            ],
+                            sourceMap: true
+                        }
+                    },
+                ],
+            },
         ]
     },
     plugins: [
